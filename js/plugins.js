e0cb29a4840594bd9a61df7e208465c54dbc51d9
Chase <chasefreeman13@gmail.com>
2016-10-18 16:57:30 -0400

    almost there

--- js/plugins.js
+++ js/plugins.js
@@ -10,25 +10,54 @@ window.log = function f(){ log.history = log.history || []; log.history.push(arg
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
 
 (function ( $ ) {
+
+  // fullQuotesArray = []
+  // fullQuotes = $(this).each(function(){
+  //   fullQuotesArray.push($(this).text());
+  // });
+  //   for (i = 0; i < fullQuotesArray.length; i++){
+  //     var fullQuote = $(this[i])
+  //     $(this[i]).removeClass('collapsedQuote')
+  //     $(this[i]).addClass('fullQuote')
+  //   }
+
+
   $.fn.collapser = function() {
     // extract sources:
     var sources = $(this).find('a')
-    // console.log(sources);
-    // Iterate through Li's, push quotes to array
+    // Iterate through li's, push quotes to array
     var quotesArray = []
-    var quotes = $(this).each(function(){
-      quotesArray.push($(this).text());
+    var quotes = $(this).each(function(i){
+      quotesArray.push($(this).addClass('quoteClass').text());
     })
     for (i = 0; i < quotesArray.length; i++){
       var source = sources.eq(i).html();
-      sourceHref = sources.eq(i).attr('href')
-      var quote = quotesArray[i];
+      var sourceHref = sources.eq(i).attr('href')
 
-      $(this[i]).each(function(){
-      $(this).html($.trim(quote).substring()
-      .split(" ").slice(0, 15).join(" ") + "... -- " + "<a href=" + sourceHref + "target='_blank' rel='nofollow'>" + source + "</a>" + "<br>" + "<a href='#'>" + 'Read More' + "</a>" + "<br>")
-        })
-    };
+
+    var collapsedQuotes = $(".quoteClass")
+    collapsedQuotes.each(function(i, val){
+      quote = $(this).text();
+
+      if (quote.split(" ").length < 15) return;
+
+      var readLessHtml = "<span style = display:none; class='readLess'><a href=# target='_blank' rel='nofollow'>" + source + "</a>" + "<br>" + "<a href='#' id='readLess'>" + 'Read Less' + "</a>" + "</span>"
+
+      var shortQuote =  $(this).html(
+        $.trim(quote).substring().split(" ").slice(0, 15).join(" ") + "... -- " + "<span class='readMore'><a href=# target='_blank' rel='nofollow'>" + source + "</a>" + "<br>" + "<a href='#' id='readMore'>" + 'Read More' + "</a>" + "</span>" + readLessHtml
+      );
+
+      $(".readMore").click(function(e){
+        e.preventDefault()
+        $(this).parent().text(quotesArray[i])
+      });
+
+
+      $(".readLess", collapsedQuotes).click(function(event){
+
+      });
+    })
+}
 
   };
-}( jQuery ));
+}(jQuery));
